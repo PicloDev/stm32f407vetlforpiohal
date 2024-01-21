eb62cd6ff20345c9b0ac3d496441923b9e84fa11
Piccolo <piccolodev@outlook.com>
2024-01-21 18:51:03 +0800

    first commit

---
+++ Core/Inc/usart.h
@@ -0,0 +1,64 @@
+/**
+  ******************************************************************************
+  * File Name          : USART.h
+  * Description        : This file provides code for the configuration
+  *                      of the USART instances.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __usart_H
+#define __usart_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN Private defines */
+#define RXMAXNUM 200                     // 接收缓冲区大�?
+  extern uint8_t rx_buffer[RXMAXNUM];    // 接收数组
+  extern volatile uint8_t rx_len;        // 接收到的数据长度
+  extern volatile uint8_t recv_end_flag; // 接收结束标志�?
+  extern UART_HandleTypeDef huart1;
+  extern DMA_HandleTypeDef hdma_usart1_rx;
+  extern DMA_HandleTypeDef hdma_usart1_tx;
+/* USER CODE END Private defines */
+
+void MX_USART1_UART_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ usart_H */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
